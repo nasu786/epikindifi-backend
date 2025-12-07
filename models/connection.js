a8d8d155c79a8cb1108d2f1308dfd452a472ea82
Mohamed Nasrutheen Sulaiman <msulaiman@msystechnologies.com>
2025-12-08 03:21:26 +0530

    Code Updated

--- models/connection.js
+++ models/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/epikindifi");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
     
     mongoose.connection.on("error", (err) => {
